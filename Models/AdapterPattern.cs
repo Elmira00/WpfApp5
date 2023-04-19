14f8f6ac3ce336eee0db5e738012c065563dd898
Ahmed_pz48 <Ahmed_pz48@itstep.edu.az>
2023-04-19 04:57:21 +0400

    Add project files.

---
+++ Models/AdapterPattern.cs
@@ -0,0 +1,178 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Xml.Serialization;
+using WpfApp5.Models;
+
+namespace WpfApp5.ViewModels
+{
+
+    interface IAdapter
+    {
+        void Write();
+    }
+
+    class JSON_Adapter : IAdapter
+    {
+        JSON_File _JSON_File;
+
+        public JSON_Adapter(JSON_File JSON_File)
+        {
+            _JSON_File = JSON_File;
+        }
+        public void Write()
+        {
+            _JSON_File.JSON_Serialize();
+        }
+    }
+
+
+    class XML_Adapter : IAdapter
+    {
+        XML_File _XML_File;
+
+
+        public XML_Adapter(XML_File XML_File)
+        {
+            _XML_File = XML_File;
+        }
+        public void Write()
+        {
+            _XML_File.XML_Serialize();
+        }
+    }
+
+
+
+    class JSON_File : BaseViewModel
+    {
+
+        private ObservableCollection<User> _user_List;
+        public ObservableCollection<User> User_List
+        {
+            get { return _user_List; }
+            set { _user_List = value; OnPropertyChanged(); }
+        }
+
+
+        private User _user;
+
+        public User User
+        {
+            get { return _user; }
+            set { _user = value; OnPropertyChanged(); }
+        }
+
+        public MainWindow MainWindows { get; set; }
+
+        public AppViewModel AppViewModel { get; set; }
+
+        public JSON_File()
+        {
+
+        }
+
+        public void JSON_Serialize()
+        {
+
+            var serializer = new JsonSerializer();
+            using (var sw = new StreamWriter($@"UserData.json", true))
+            {
+                using (var jw = new JsonTextWriter(sw))
+                {
+                    jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    serializer.Serialize(jw, User_List);
+                }
+            }
+
+        }
+
+
+
+    }
+
+
+
+    class XML_File : BaseViewModel
+    {
+
+        private ObservableCollection<User> _user_List;
+        public ObservableCollection<User> User_List 
+        { 
+            get { return _user_List; } 
+            set { _user_List = value; OnPropertyChanged(); }
+        }
+
+
+        private User _user;
+
+        public User User 
+        { 
+            get { return _user; } 
+            set { _user = value; OnPropertyChanged(); } 
+        }
+
+
+        public MainWindow MainWindows { get; set; }
+
+        public AppViewModel appViewModel { get; set; }
+
+        public XML_File()
+        {
+
+        }
+
+
+
+        public void XML_Serialize()
+        {
+
+            var xml = new XmlSerializer(typeof(ObservableCollection<User>));
+            using (var fs = new FileStream($@"UserData.xml", FileMode.OpenOrCreate))
+            {
+                xml.Serialize(fs, User_List);
+            }
+        }
+
+
+    }
+
+
+
+
+    class Converter
+    {
+        private readonly IAdapter _adapter;
+
+        public Converter(IAdapter adapter)
+        {
+            _adapter = adapter;
+        }
+
+        public void WriteFile()
+        {
+            _adapter.Write();
+        }
+
+    }
+
+
+    class Application_File
+    {
+        private readonly Converter _converter;
+
+        public Application_File(IAdapter adapter)
+        {
+            _converter = new Converter(adapter);
+        }
+
+        public void Start()
+        {
+            _converter.WriteFile();
+        }
+
+    }
+
+
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp5.ViewModels;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new AppViewModel() { MainWindow = this };
+        }
+
+        private void NameTxtBx_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (NameTxtBx.Text == "Name")
+            {
+                NameTxtBx.Text = null;
+
+
+
+                Color color1 = new Color();
+                color1 = Color.FromArgb(255, 37, 191, 191);
+
+                NameTxtBx.Foreground = new SolidColorBrush(color1);
+            }
+        }
+
+        private void NameTxtBx_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (NameTxtBx.Text == "")
+            {
+
+                Color color2 = new Color();
+                color2 = Color.FromArgb(255, 110, 127, 128);
+
+                NameTxtBx.Text = "Name";
+                NameTxtBx.Foreground = new SolidColorBrush(color2);
+            }
+        }
+
+        private void SurnameTxtBx_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (SurnameTxtBx.Text == "Surname")
+            {
+                SurnameTxtBx.Text = null;
+
+
+
+                Color color1 = new Color();
+                color1 = Color.FromArgb(255, 37, 191, 191);
+
+                SurnameTxtBx.Foreground = new SolidColorBrush(color1);
+
+
+            }
+        }
+
+        private void SurnameTxtBx_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (SurnameTxtBx.Text == "")
+            {
+
+                Color color2 = new Color();
+                color2 = Color.FromArgb(255, 110, 127, 128);
+
+                SurnameTxtBx.Text = "Surname";
+                SurnameTxtBx.Foreground = new SolidColorBrush(color2);
+            }
+        }
+
+        private void EmailTxtBx_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (EmailTxtBx.Text == "Email")
+            {
+                EmailTxtBx.Text = null;
+
+
+
+                Color color1 = new Color();
+                color1 = Color.FromArgb(255, 37, 191, 191);
+
+                EmailTxtBx.Foreground = new SolidColorBrush(color1);
+
+
+            }
+        }
+
+        private void EmailTxtBx_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (EmailTxtBx.Text == "")
+            {
+
+                Color color2 = new Color();
+                color2 = Color.FromArgb(255, 110, 127, 128);
+
+                EmailTxtBx.Text = "Email";
+                EmailTxtBx.Foreground = new SolidColorBrush(color2);
+            }
+        }
+    }
+}
